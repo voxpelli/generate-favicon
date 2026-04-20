54b13a78807b54b62230b88a168440a34368d8e7
Pelle Wessman <pelle@kodfabrik.se>
2026-04-20 16:35:24 +0200

    chore(deps): update most dependencies

--- cli.js
+++ cli.js
@@ -40,7 +40,7 @@ try {
     }
   }
 
-  // eslint-disable-next-line no-console, n/no-unsupported-features/node-builtins
+  // eslint-disable-next-line no-console
   console.error(`${styleText('bgRed', errorTitle + ':')} ${errorMessage}`);
   if (errorBody) {
     // eslint-disable-next-line no-console
